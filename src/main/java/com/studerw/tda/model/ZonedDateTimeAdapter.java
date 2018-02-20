66d507a2a6a16d2b9c8aeda77892e08246c6d02e
William Studer <bill.studer@useitc.com>
2018-02-20 07:34:38 -0500

    adding Java 8 DateTime Support

---
+++ src/main/java/com/studerw/tda/model/ZonedDateTimeAdapter.java
@@ -0,0 +1,23 @@
+package com.studerw.tda.model;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * TD Ameritrade date format: 2017-12-06 23:06:14 EST
+ */
+public class ZonedDateTimeAdapter extends XmlAdapter<String, ZonedDateTime> {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+    @Override
+    public String marshal(ZonedDateTime v) throws Exception {
+        return formatter.format(v);
+    }
+
+    @Override
+    public ZonedDateTime unmarshal(String v) throws Exception {
+        return ZonedDateTime.parse(v, formatter);
+    }
+}
--- src/main/java/com/studerw/tda/model/package-info.java
+++ src/main/java/com/studerw/tda/model/package-info.java
@@ -1,10 +1,15 @@
-@XmlJavaTypeAdapter(value = CalendarAdapter.class, type = Calendar.class)
+@XmlJavaTypeAdapters({
+    @XmlJavaTypeAdapter(value = CalendarAdapter.class, type = Calendar.class),
+    @XmlJavaTypeAdapter(value = ZonedDateTimeAdapter.class, type = ZonedDateTime.class)
+})
 @XmlAccessorType(XmlAccessType.FIELD)
 //@javax.xml.bind.annotation.XmlSchema(namespace = "http://tdameritrade.api.com/")
 package com.studerw.tda.model;
 
+import java.time.ZonedDateTime;
+import java.util.Calendar;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import java.util.Calendar;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
 
