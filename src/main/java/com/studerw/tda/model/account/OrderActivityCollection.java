86ee9025be71db0e33eaf6ec254e9e7004fc1f9d
PJ <me@pauljulius.com>
2020-01-25 17:06:58 -0800

    Ran into issues trying to run mvn failsafe:integration-test. The Failsafe plugin version caused an error, so upgraded to 2.22.1. The Bond cusip \'06405VDP1\' is no longer a valid cusip. And apparently TD has changed their OrderActivityCollection json a bit, so I added the fields.

--- src/main/java/com/studerw/tda/model/account/OrderActivityCollection.java
+++ src/main/java/com/studerw/tda/model/account/OrderActivityCollection.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class OrderActivityCollection implements Serializable {
@@ -11,6 +14,15 @@ public class OrderActivityCollection implements Serializable {
 
   @JsonProperty("activityType")
   private ActivityType activityType;
+  @JsonProperty("executionType")
+  private OrderActivity.ExecutionType executionType;
+  @JsonProperty("quantity")
+  private BigDecimal quantity;
+  @JsonProperty("orderRemainingQuantity")
+  private BigDecimal orderRemainingQuantity;
+  @JsonProperty("executionLegs")
+  private List<ExecutionLeg> executionLegs = new ArrayList<ExecutionLeg>();
+
 
   public ActivityType getActivityType() {
     return activityType;
