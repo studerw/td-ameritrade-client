c5d6d5acfd3a5861ddaa327dd265c0ee1ab41280
Victor Philip <vphilipnyc@gmail.com>
2020-05-25 21:02:52 -0400

    slightly more conventional style, removed logger

--- src/main/java/com/studerw/tda/http/cookie/UserAgentInterceptor.java
+++ src/main/java/com/studerw/tda/http/cookie/UserAgentInterceptor.java
@@ -9,13 +9,15 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 public class UserAgentInterceptor implements Interceptor {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserAgentInterceptor.class);
+
     private static final String UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        Request request = chain.request();
-        Request userAgentReq = request.newBuilder().header("User-Agent", UA).build();
+        Request userAgentReq = chain.request()
+                .newBuilder()
+                .header("User-Agent", UA)
+                .build();
         return chain.proceed(userAgentReq);
     }
 }
