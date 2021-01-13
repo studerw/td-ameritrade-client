af1340fc2f06290c6720f0fb7bc5eabf1b391a39
William Studer <bill.studer@sent.com>
2021-01-13 00:47:06 -0500

    fix maven.yml syntax error

--- src/test/java/com/studerw/tda/client/GetUserPrincipalsTestIT.java
+++ src/test/java/com/studerw/tda/client/GetUserPrincipalsTestIT.java
@@ -2,7 +2,6 @@ package com.studerw.tda.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.studerw.tda.model.user.Preferences;
 import com.studerw.tda.model.user.UserPrincipals;
 import com.studerw.tda.model.user.UserPrincipals.Field;
 import java.util.Map;
@@ -37,15 +36,19 @@ public class GetUserPrincipalsTestIT extends BaseTestIT {
 
   @Test
   public void testGetUserPrincipalsNoPrefs() {
-    final UserPrincipals userPrincipals = httpTdaClient.getUserPrincipals(Field.streamerConnectionInfo, Field.surrogateIds, Field.streamerSubscriptionKeys, Field.preferences);
+    final UserPrincipals userPrincipals = httpTdaClient
+        .getUserPrincipals(
+            Field.streamerConnectionInfo,
+            Field.surrogateIds,
+            Field.streamerSubscriptionKeys,
+            Field.preferences
+        );
     assertThat(userPrincipals).isNotNull();
     assertThat(userPrincipals.getStreamerSubscriptionKeys()).isNotNull();
     assertThat(userPrincipals.getStreamerInfo()).isNotNull();
-    final Map<String, String> surrogateIds = userPrincipals.getAccounts().get(0)
-        .getSurrogateIds();
-    assertThat(surrogateIds).isNotNull();
-    final Preferences preferences = userPrincipals.getAccounts().get(0).getPreferences();
-    assertThat(preferences).isNotNull();
+    assertThat(userPrincipals.getAccounts().get(0)
+        .getSurrogateIds()).isNotNull();
+    assertThat(userPrincipals.getAccounts().get(0).getPreferences()).isNotNull();
     LOGGER.debug("{}", userPrincipals);
   }
 
